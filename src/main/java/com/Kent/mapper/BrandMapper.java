7f4fb217edc3747c2242fc4cbea4e6f5c2e9822c
Zijun <s410126080@gmail.com>
2023-04-01 23:04:12 +0800

    建立 BrandMapper interface

---
+++ src/main/java/com/Kent/mapper/BrandMapper.java
@@ -0,0 +1,16 @@
+package com.Kent.mapper;
+
+
+import com.Kent.pojo.Brand;
+
+import java.util.List;
+
+public interface BrandMapper {
+
+    /**
+     * 查詢所有
+     */
+
+    public List<Brand> selectAll();
+}
+
