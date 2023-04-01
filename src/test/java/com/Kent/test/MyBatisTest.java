d17fed981b09bd652a237186bf701cca4eae3819
Zijun <s410126080@gmail.com>
2023-04-01 23:06:33 +0800

    建立測試用檔案，建立 brand selectAll() 查詢全部資料功能

--- src/test/java/com/Kent/test/MyBatisTest.java
+++ src/test/java/com/Kent/test/MyBatisTest.java
@@ -1,5 +1,37 @@
 package com.Kent.test;
 
+import com.Kent.mapper.BrandMapper;
+import com.Kent.pojo.Brand;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 public class MyBatisTest {
 
+    @Test
+    public void testSelectAll() throws IOException {
+        // 1. 取得 SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 2. 取得 sqlSessionFactory 物件
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3. 取得 Mapper interface 的代理物件
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        // 4. 執行方法
+        List<Brand> brands = brandMapper.selectAll();
+        System.out.println(brands);
+
+        // 5. 釋放資源
+        sqlSession.close();
+    }
 }
