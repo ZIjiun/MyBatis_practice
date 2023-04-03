09198c02e9deea1def9b09843feef242cc8453cb
Zijun <s410126080@gmail.com>
2023-04-03 11:04:55 +0800

    MyBatis 中參數的封裝、傳遞原理

--- src/main/java/com/Kent/mapper/UserMapper.java
+++ src/main/java/com/Kent/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.Kent.mapper;
 
 import com.Kent.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface UserMapper {
@@ -10,5 +12,38 @@ public interface UserMapper {
     List<User> selectAll();
 
     User selectById(int id);
+
+    /**
+     * MyBatis 參數封裝
+        * 單個參數:
+            1. POJO 類型: 直接使用. 屬性名 (使用 class 裡面的 get 方法取得) 和 sql 語句參數佔位符名稱　一致
+            2. Map 集合:　直接使用. 鍵名 和 sql 語句參數佔位符名稱　一致
+            3. Collection: 封裝為 Map 集合. 可以使用 @Param 註解，用來替換 Map 集合中預設的 arg 鍵名
+                map.put("collection", collection 集合);
+                map.put("arg0", collection 集合);
+            4. List: 封裝為 Map 集合. 可以使用 @Param 註解，用來替換 Map 集合中預設的 arg 鍵名
+                 map.put("collection", list 集合);
+                 map.put("list", list 集合);
+                 map.put("arg0", list 集合);
+            5. Array: 封裝為 Map 集合. 可以使用 @Param 註解，用來替換 Map 集合中預設的 arg 鍵名
+                 map.put("array", array);
+                 map.put("arg0", array);
+            6. 其他類型: example => int，直接使用
+        * 多個參數: 封裝為 Map 集合，可以使用 @Param 註解，用來替換 Map 集合中預設的 arg 鍵名
+            map.put("arg0", 參數值1)
+            map.put("param1", 參數值1)
+            map.put("arg1", 參數值2)
+            map.put("param2", 參數值2)
+            ------------------------------@Param("username")
+             map.put("username", 參數值1)
+             map.put("param1", 參數值1)
+             map.put("arg1", 參數值2)
+             map.put("param2", 參數值2)
+     */
+
+    User select(@Param("username") String username, @Param("password") String password);
+
+    // 用來示範 collection 參數的傳遞過程
+    //    User select(Collection collection);
 }
 
---
+++ src/test/java/com/Kent/test/UserMapperTest.java
@@ -0,0 +1,48 @@
+package com.Kent.test;
+
+import com.Kent.mapper.BrandMapper;
+import com.Kent.mapper.UserMapper;
+import com.Kent.pojo.Brand;
+import com.Kent.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+public class UserMapperTest {
+    @Test
+    public void testSelect() throws IOException {
+        // 1. 取得 SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 2. 取得 sqlSessionFactory 物件
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3. 取得 Mapper interface 的代理物件
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        // 4. 執行方法
+        String username = "李四";
+        String password = "123";
+
+
+        User user = userMapper.select(username, password);
+
+//        用來示範 collection 參數傳遞過程，但是因為 set 裡面沒有參數，到 sql 語句時，會因為找不到 sql 的字段佔位符所以出錯
+//        User user = userMapper.select(new HashSet());
+
+        System.out.println(user);
+
+        // 5. 釋放資源
+        sqlSession.close();
+    }
+}
